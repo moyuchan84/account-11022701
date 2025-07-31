a2202a279051dcb405db335b404f54cacae9e57f
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 09:03:25 +0900

    [feature] p.17 add deposit function

--- account.py
+++ account.py
@@ -1,3 +1,6 @@
 class Account:
     def __init__(self,balance):
         self._balance = balance
+
+    def deposit(self, value):
+        self._balance = 10500
--- test_account.py
+++ test_account.py
@@ -9,3 +9,9 @@ def test_account_init_10000_won():
     account = Account(10000)
     ret = account._balance
     assert ret == 10000
+
+
+def test_deposit_and_confirmation():
+    account = Account(10000)
+    account.deposit(500)
+    assert account._balance == 10500
