51c56a557291cb0cf5c35a9cd76ae1036843cf47
yuchan <mo.yuchan.84@gmail.com>
2025-07-31 09:04:22 +0900

    [refactor] rename method param from value to money and add money to balance

--- account.py
+++ account.py
@@ -2,5 +2,5 @@ class Account:
     def __init__(self,balance):
         self._balance = balance
 
-    def deposit(self, value):
-        self._balance = 10500
+    def deposit(self, money):
+        self._balance += money
